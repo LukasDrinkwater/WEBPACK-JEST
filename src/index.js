251b85b365090df0114640ecb33552069666be45
Lukas <lukasdrinkwater97@gmail.com>
2023-07-06 13:48:46 +0100

    working now, needed babel-loader adding

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ function capitalise(word) {
   return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
-export { checkIfArrayLegal, capitalise };
+export {  capitalise };
--- webpack.config.js
+++ webpack.config.js
@@ -26,9 +26,11 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        exclude: ["node_modules"],
-        use: ["babel-loader"],
+        test: /\.js?$/i,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+        },
       },
       {
         test: /\.css$/,
